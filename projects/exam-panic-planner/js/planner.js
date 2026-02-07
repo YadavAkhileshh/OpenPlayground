63ff44942a33aa894ad2e88b27848d265f31a71a
Varri Chandrasekhar <varrichandrasekhar5@gmail.com>
2026-02-07 18:28:37 +0530

    add progress calculation, display it, and panic mode

--- projects/exam-panic-planner/js/planner.js
+++ projects/exam-panic-planner/js/planner.js
@@ -1,5 +1,8 @@
 const user = JSON.parse(localStorage.getItem("user"));
 const plan = JSON.parse(localStorage.getItem("plan"));
+const syllabus =
+  JSON.parse(localStorage.getItem("syllabus"));
+
 
 if(!user || !plan){
   window.location.href="setup.html";
@@ -46,3 +49,16 @@ document.getElementById("saveBtn").addEventListener("click",()=>{
   localStorage.setItem("plan",JSON.stringify(plan));
   alert("Progress saved");
 });
+
+const progress = calculateProgress(plan);
+
+document.getElementById("progressBox")
+.innerText = `Progress: ${progress}%`;
+
+const panic = checkPanic(user, syllabus);
+
+if(panic){
+  const banner=document.getElementById("panicBanner");
+  banner.innerText="🚨 Panic Mode: You are falling behind!";
+  banner.style.color="red";
+}
